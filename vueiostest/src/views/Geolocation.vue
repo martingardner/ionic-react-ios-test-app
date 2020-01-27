1b26471a2a91c7ce8ed04f1213416f0b04bb7a6a
martin gardner II <martingardnerii@gmail.com>
2020-01-27 08:17:07 -0600

    push geolocation code to test on tablet

--- vueiostest/src/views/Geolocation.vue
+++ vueiostest/src/views/Geolocation.vue
@@ -8,6 +8,7 @@
         <div>lat: {{ lat }}</div>
         <div>long: {{ lng }}</div>
         <div>speed: {{ spd }}</div>
+        <div v-if="this.watchErr">watchPosition error: {{ watchErr }}</div>
       </div>
     </ion-content>
   </div>
@@ -25,7 +26,9 @@ export default {
     return {
       lat: 0,
       lng: 0,
-      spd: 0
+      spd: 0,
+      watchErr: "",
+      watchId: null
     };
   },
   components: {
@@ -34,33 +37,51 @@ export default {
   methods: {
     geolocation() {
       let $data = this;
+      let $active = false;
+      console.log("$active", $active);
       async function getCoords() {
+        $active = true;
         const coordinates = await Geolocation.getCurrentPosition();
+
         console.log("Current", coordinates);
         console.log("Current coords", coordinates.coords);
         console.log("latitude", coordinates.coords.latitude);
         console.log("current data latitude", $data.lat);
+        console.log("after async $active", $active);
 
         $data.lat = coordinates.coords.latitude;
         $data.lng = coordinates.coords.longitude;
         $data.spd = coordinates.coords.speed || 0;
+        $active = false;
       }
-
-      getCoords();
+      if (!$active) {
+        getCoords();
+      }
     },
-    geolocationInterval() {
-      /*
-      setInterval(() => {
-        console.log("hit");
-        this.geolocation();
-      }, 1000);
-      */
+    geolocationWatch() {
+      let id = navigator.geolocation.watchPosition(
+        this.geolocation,
+        this.watchError,
+        {
+          enableHighAccuracy: true,
+          timeout: 1000,
+          maximumAge: 0
+        }
+      );
+
+      this.watchId = id;
+      console.log("hit");
+    },
+    watchError(err) {
+      console.log("err", err);
+      this.watchErr = err.message;
     }
   },
   created() {
     console.log("created");
-    this.geolocation();
-    //this.geolocationInterval();
+    //this.geolocation();
+    this.geolocationWatch();
+    //this.geolocationWatch();
   }
 };
 </script>
