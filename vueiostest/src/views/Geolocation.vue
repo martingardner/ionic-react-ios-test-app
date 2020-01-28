376e3a5e7a30437f261ebc184e1734d5e381a389
martin gardner II <martingardnerii@gmail.com>
2020-01-28 07:45:18 -0600

    trying getCurrentPosition vs watchPosition for permissions issue

--- vueiostest/src/views/Geolocation.vue
+++ vueiostest/src/views/Geolocation.vue
@@ -41,7 +41,7 @@ export default {
         const coordinates = await Geolocation.getCurrentPosition();
 
         //console.log("Current", coordinates);
-        //console.log("Current coords", coordinates.coords);
+        console.log("Current coords", coordinates.coords);
         //console.log("latitude", coordinates.coords.latitude);
         //console.log("current data latitude", $data.lat);
 
@@ -77,7 +77,8 @@ export default {
     }
   },
   created() {
-    this.geolocationWatch();
+    //this.geolocationWatch();
+    this.geolocation();
   }
 };
 </script>
