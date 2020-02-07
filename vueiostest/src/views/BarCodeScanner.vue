de24816ad6b42b03a584192ad7829c4df0e27471
martin gardner II <martingardnerii@gmail.com>
2020-02-07 11:32:55 -0600

    add empty method call for easier git reset for testing libraries

--- vueiostest/src/views/BarCodeScanner.vue
+++ vueiostest/src/views/BarCodeScanner.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="ion-page">
-        <ion-header></ion-header>
-        <ion-content class="ion-padding">
-            <Navigation />
-            <h1>BarCode Scanner</h1>
-        </ion-content>
-    </div>
+  <div class="ion-page">
+    <ion-header></ion-header>
+    <ion-content class="ion-padding">
+      <Navigation />
+      <h1>BarCode Scanner</h1>
+      <button @click="scan">Scan</button>
+    </ion-content>
+  </div>
 </template>
 
 <script>
-import Navigation from '@/components/Navigation.vue';
+import Navigation from "@/components/Navigation.vue";
 
 export default {
-    name: "BarCodeScanner",
-    components: {
-        Navigation
+  name: "BarCodeScanner",
+  methods: {
+    scan() {
+      console.log("scan hit");
     }
-}
+  },
+  components: {
+    Navigation
+  }
+};
 </script>
-
